3a0f2d0f30a6dbc0afb30b263088bd1be9b5bb8a
Orlando Challenger <orlandofat@gmail.com>
2020-05-11 18:22:33 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -10,8 +10,9 @@ end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  {:name => "Grace Hopper"}
 end
-
+pioneer
 
 def id_hash_generator(number)
   # return a hash with a key :id assigned to the provided number
